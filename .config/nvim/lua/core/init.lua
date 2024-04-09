05da1ee807f2fd26d977a3fdaeba034bed26526e
kqnade <git@k4na.de>
2024-04-09 23:24:47 +0900

    nvim settings wip

---
+++ .config/nvim/lua/core/init.lua
@@ -0,0 +1,15 @@
+-- Core section
+
+-- modules load conf
+require("modules")
+
+-- load plugins
+require("plugins")
+
+-- load keymaps
+require("keymaps")
+
+-- load must settings
+require("core.settings")
+
+
