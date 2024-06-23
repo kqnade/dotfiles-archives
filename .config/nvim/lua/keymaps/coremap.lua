cce574c4042387808e5963c9dd549c2199f93e9c
kqnade <git@k4na.de>
2024-06-24 00:30:32 +0900

    buffer command add

--- .config/nvim/lua/keymaps/coremap.lua
+++ .config/nvim/lua/keymaps/coremap.lua
@@ -23,6 +23,9 @@ local core_map = {
 	["n|tk"] = map_cr("tabnext"):with_noremap():with_silent():with_desc("tab: Move to next tab"),
 	["n|tj"] = map_cr("tabprevious"):with_noremap():with_silent():with_desc("tab: Move to previous tab"),
 	["n|to"] = map_cr("tabonly"):with_noremap():with_silent():with_desc("tab: Only keep current tab"),
+  ["n|<A-]>"] = map_cr("bnext"):with_silent():with_noremap():with_desc("Buffer: Move to next buffer"),
+  ["n|<A-[>"] = map_cr("bprevious"):with_silent():with_noremap():with_desc("Buffer: Move to previous buffer"),
+
 	-- Insert mode
 	["i|<C-u>"] = map_cmd("<C-G>u<C-U>"):with_noremap():with_desc("edit: Delete previous block"),
 	["i|<C-b>"] = map_cmd("<Left>"):with_noremap():with_desc("edit: Move cursor to left"),
